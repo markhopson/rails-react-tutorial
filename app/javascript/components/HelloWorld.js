c71a873cdd2231b7dc3cfa1566f0b366405b8871
Mark Hopson <mark.hopson@wishabi.com>
2019-03-05 09:40:59 -0500

    Dispatch an Action from HelloWorld

--- app/javascript/components/HelloWorld.js
+++ app/javascript/components/HelloWorld.js
@@ -1,16 +1,34 @@
 import React from "react"
-import PropTypes from "prop-types"
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+const GET_THINGS_REQUEST = 'GET_THINGS_REQUEST';
+
+function getThings() {
+  console.log('getThings() Action!!')
+  return {
+    type: GET_THINGS_REQUEST
+  };
+};
+
 class HelloWorld extends React.Component {
   render () {
     return (
       <React.Fragment>
         Greeting: {this.props.greeting}
+
+        <button className="getThingsBtn" onClick={() => this.props.getThings()}>getThings</button>
+
       </React.Fragment>
     );
   }
 }
 
-HelloWorld.propTypes = {
-  greeting: PropTypes.string
-};
-export default HelloWorld
+const structuredSelector = createStructuredSelector({
+  things: state => state.things,
+}); 
+
+const mapDispatchToProps = { getThings };
+
+export default connect(structuredSelector, mapDispatchToProps)(HelloWorld);
+
